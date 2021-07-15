fe79cce06f35000673df72e0664be6c700b8b96d
mahmoudalfawair <84728207+mahmoudalfawair@users.noreply.github.com>
2021-07-15 12:05:21 +0300

    Add files via upload

---
+++ task15JulA.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+cout << "Enter 2 integers number \n";
+int x1;
+cout << "enter the first interger number \n";
+cin >> x1;
+int x2 ;
+cout << "Enter the second interger number \n";
+cin >> x2 ;
+if(x1 == x2)
+cout << x1 << " = " <<  x2 << endl;
+else 
+cout << x1 << " doesn't equal to " << x2 << endl;
+return 0;
+
+
+
+
+}
+
+
+
---
+++ task15JulB.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int x ;
+cin >> x ;
+if(x%2==1)
+cout << x << " is odd" << endl ;
+else 
+cout << x << " is even" << endl ;
+return 0;
+
+
+}
